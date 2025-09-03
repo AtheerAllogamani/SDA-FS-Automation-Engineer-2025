7d0df7d3c80d9f7d1e2062a33f937f3f6c18252e
Atheer Allogamani <100247724+AtheerAllogamani@users.noreply.github.com>
2025-09-03 07:15:20 +0300

    Update SeleniumTask2.java

--- Selenium/SeleniumTask2.java
+++ Selenium/SeleniumTask2.java
@@ -25,7 +25,7 @@ public class Main {
         // Minimize the page
         driver.manage().window().minimize();
 
-        // Wait 5 seconds in the icon state and maximize the page
+        // Wait 5 seconds in the icon state and maximize the page (Thread.sleep(5000) was not working for me)
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
 
