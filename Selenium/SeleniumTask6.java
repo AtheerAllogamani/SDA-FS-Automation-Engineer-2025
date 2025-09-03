3c92182ab7d1cb9cb48dd0a55663d28b21fb9403
Atheer Allogamani <100247724+AtheerAllogamani@users.noreply.github.com>
2025-09-03 07:08:04 +0300

    Update and rename Main.java to SeleniumTask6.java

--- Selenium/SeleniumTask6.java
+++ Selenium/SeleniumTask6.java
@@ -3,9 +3,6 @@ package com.github.AtheerAllogamani;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-
-
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
