7136163da18dc7552587e5e99d58956c7c800d12
Atheer Allogamani <100247724+AtheerAllogamani@users.noreply.github.com>
2025-09-03 06:42:20 +0300

    Update SeleniumTask1.java

--- Selenium/SeleniumTask1.java
+++ Selenium/SeleniumTask1.java
@@ -10,16 +10,22 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new EdgeDriver();
+        
         // Navigate to URL: https://clarusway.com
-        driver.navigate().to("https://clarusway.com/");
+        driver.navigate().to("https://clarusway.com");
+        
         // Navigate to URL: http://amazon.com
-        driver.navigate().to("http://amazon.com/");
+        driver.navigate().to("http://amazon.com");
+        
         // Come back to the Clarusway using the back command.
         driver.navigate().back();
+        
         // Again go back to the amazon website using forward command
         driver.navigate().forward();
+        
         // Refresh the Browser using refresh command
         driver.navigate().refresh();
+        
         // Close the Browser
         driver.quit();
 
