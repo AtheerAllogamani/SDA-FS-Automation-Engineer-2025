2e8108731243f7cd893f59a436c6cd729aeb533c
Atheer Allogamani <100247724+AtheerAllogamani@users.noreply.github.com>
2025-09-10 08:33:08 +0300

    Update BasicDropDownOperationsTest.java

--- SeleniumDropDownSynchronization/BasicDropDownOperationsTest.java
+++ SeleniumDropDownSynchronization/BasicDropDownOperationsTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
@@ -21,6 +21,8 @@ public class BasicDropDownOperationsTest {
         driver= new EdgeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        dropdown = new Select(driver.findElement(By.id("dropdown")));
     }
 
     @Test
