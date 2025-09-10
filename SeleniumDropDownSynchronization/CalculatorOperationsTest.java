5729ef19f7f223652bf691444bba9d37a271fe4c
Atheer Allogamani <100247724+AtheerAllogamani@users.noreply.github.com>
2025-09-10 08:21:46 +0300

    Create CalculatorOperationsTest.java

---
+++ SeleniumDropDownSynchronization/CalculatorOperationsTest.java
@@ -0,0 +1,42 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorOperationsTest {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://testpages.eviltester.com/styled/calculator");
+    }
+
+    @Test
+    public void testCalculatorOperations() {
+        // Input numbers
+        driver.findElement(By.id("number1")).sendKeys("10");
+        driver.findElement(By.id("number2")).sendKeys("5");
+
+        // Perform calculations
+        String[] operations = {"add", "subtract", "multiply", "divide"};
+        String[] expectedResults = {"15", "5", "50", "2"};
+        
+        for (int i = 0; i < operations.length; i++) {
+            driver.findElement(By.id(operations[i])).click();
+            String result = driver.findElement(By.id("answer")).getText();
+            assertEquals("Result for " + operations[i] + " is incorrect", expectedResults[i], result);
+        }
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
