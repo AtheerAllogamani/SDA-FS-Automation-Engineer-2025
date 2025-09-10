8ee91efd1a0ee6c4c442eacc23d167bdb4622685
Atheer Allogamani <100247724+AtheerAllogamani@users.noreply.github.com>
2025-09-10 08:33:36 +0300

    Update CalculatorOperationsTest.java

--- SeleniumDropDownSynchronization/CalculatorOperationsTest.java
+++ SeleniumDropDownSynchronization/CalculatorOperationsTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,6 +16,7 @@ public class CalculatorOperationsTest {
         driver= new EdgeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        driver.get("https://testpages.eviltester.com/styled/calculator");
     }
 
     @Test
